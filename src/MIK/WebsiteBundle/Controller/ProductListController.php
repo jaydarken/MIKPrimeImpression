1758ed08e359ed960fc302fb1d3f40977ac2ea0f
R-jay Villegas <officialrjayv@gmail.com>
2017-11-12 15:06:34 +0800

    Add show product pages

--- src/MIK/WebsiteBundle/Controller/ProductListController.php
+++ src/MIK/WebsiteBundle/Controller/ProductListController.php
@@ -4,17 +4,38 @@ namespace MIK\WebsiteBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use MIK\WebsiteBundle\Entity\Product;
 
 class ProductListController extends Controller
 {
     /**
      * @Route("/product/{productName}/{productType}")
      */
-    public function showProductAction()
+    public function showProductWithTypeAction($productName, $productType)
     {
-        return $this->render('MIKWebsiteBundle:ProductList:show_product.html.twig', array(
-            // ...
-        ));
+        $products = $this->getDoctrine()
+        ->getRepository(Product::class)
+        ->findRequestProductWithType($productName, $productType);
+
+        return $this->render('MIKWebsiteBundle:ProductList:show_product_with_type.html.twig', [ 
+            'products' => $products
+        ]);
     }
+
+    /**
+     * @Route("/product/{productName}")
+     */
+    public function showProductAction($productName)
+    {
+        $products = $this->getDoctrine()
+        ->getRepository(Product::class)
+        ->findRequestProduct($productName);
+
+        return $this->render('MIKWebsiteBundle:ProductList:show_product.html.twig', [ 
+            'products' => $products
+        ]);
+    }
+
 
 }
--- src/MIK/WebsiteBundle/Repository/ProductRepository.php
+++ src/MIK/WebsiteBundle/Repository/ProductRepository.php
@@ -10,4 +10,26 @@ namespace MIK\WebsiteBundle\Repository;
  */
 class ProductRepository extends \Doctrine\ORM\EntityRepository
 {
+    public function findRequestProductWithType($productName, $productType) {
+        $productName = strtoupper($productName);
+        $productType = strtoupper($productType);
+        return $this->getEntityManager()
+        ->createQuery(
+            "SELECT p FROM MIKWebsiteBundle:Product p
+            WHERE p.prodName = '$productName' and p.prodType = '$productType'
+            ORDER BY p.prodActName ASC"
+        )
+        ->getResult();
+    }
+
+    public function findRequestProduct($productName) {
+        $productName = strtoupper($productName);
+        return $this->getEntityManager()
+        ->createQuery(
+            "SELECT p FROM MIKWebsiteBundle:Product p
+            WHERE p.prodName = '$productName'
+            ORDER BY p.prodActName ASC"
+        )
+        ->getResult();
+    }
 }
